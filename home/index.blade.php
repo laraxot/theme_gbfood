34f3bf285fc171eaec8de5228b50b4db008c6bcb
marco76tv <marco.sottana@gmail.com>
2019-09-15 19:43:18 +0200

    .

--- home/index.blade.php
+++ home/index.blade.php
@@ -8,11 +8,11 @@
 	{{--@include($view.'.popular')--}}
 	@include($view.'.banner')
 	{{--
+    --}}
 	@include($view.'.what_we_do')
 	@include($view.'.how_it_works')
 	@include('pub_theme::layouts.partials.mobile_app')
     @include($view.'.iscrizione') 
-    --}}
     {{--@include($view.'.featured_restaurants')--}}
     
 @endsection
