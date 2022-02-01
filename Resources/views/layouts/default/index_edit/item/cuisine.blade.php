b69196d00782850335fd36dc9225d0d6f1e99dc5
marco76tv <marco.sottana@gmail.com>
2022-02-01 16:26:21 +0100

    .

--- Resources/views/layouts/default/index_edit/item/cuisine.blade.php
+++ Resources/views/layouts/default/index_edit/item/cuisine.blade.php
@@ -1,33 +1,30 @@
 @php
-	$cuisine_panel=Panel::get($row);
-	$edit_url=$cuisine_panel->relatedUrl(['related_name'=>'recipe','act'=>'index_edit']);
+$cuisine_panel = Panel::get($row);
+$edit_url = $cuisine_panel->relatedUrl('recipe', 'index_edit');
 @endphp
 
 <div class="menu-widget m-b-30" id="{{ $row->post_id }}">
-	<div class="widget-heading">
-		<h3 class="widget-title text-dark">
-			<b class="text-dark">{{ $row->title }}</b>
-			<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModalAjax" data-title="" 
-                            data-href="{{  $edit_url }}">
-                            <i class="fa fa-wrench"></i>
-        	</button>
-			<a class="btn btn-link pull-right" data-toggle="collapse" href="#m{{ $row->post_id }}" aria-expanded="true">
-			<i class="fa fa-angle-right pull-right"></i>
-			<i class="fa fa-angle-down pull-right"></i>
-			</a>
-			<p class="text-dark">{{ $row->subtitle }}</p>
-		</h3>
-		<div class="clearfix"></div>
-		{{--
-		@includeFirst([$view.'.item.add.recipe',$view_default.'.item.add.recipe'],['cuisine'=>$row])
-		--}}
-	</div>
-	<div class="collapse show" id="m{{ $row->post_id }}">
-		@foreach($row->recipes as $key=>$recipe)
-			@include($_layout->view_default.'.item.cuisine_x_recipe',['key'=>$key,'row'=>$recipe,'cuisine'=>$cuisine_panel])
-		@endforeach
-	</div>
+    <div class="widget-heading">
+        <h3 class="widget-title text-dark">
+            <b class="text-dark">{{ $row->title }}</b>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModalAjax" data-title=""
+                data-href="{{ $edit_url }}">
+                <i class="fa fa-wrench"></i>
+            </button>
+            <a class="btn btn-link pull-right" data-toggle="collapse" href="#m{{ $row->post_id }}"
+                aria-expanded="true">
+                <i class="fa fa-angle-right pull-right"></i>
+                <i class="fa fa-angle-down pull-right"></i>
+            </a>
+            <p class="text-dark">{{ $row->subtitle }}</p>
+        </h3>
+        <div class="clearfix"></div>
+        {{-- @includeFirst([$view.'.item.add.recipe',$view_default.'.item.add.recipe'],['cuisine'=>$row]) --}}
+    </div>
+    <div class="collapse show" id="m{{ $row->post_id }}">
+        @foreach ($row->recipes as $key => $recipe)
+            @include($_layout->view_default.'.item.cuisine_x_recipe',['key'=>$key,'row'=>$recipe,'cuisine'=>$cuisine_panel])
+        @endforeach
+    </div>
 </div>
-{{--
-@include($view.'.order_modal')
---}}
+{{-- @include($view.'.order_modal') --}}
--- Resources/views/layouts/default/index_edit/right/cuisine.blade.php
+++ Resources/views/layouts/default/index_edit/right/cuisine.blade.php
@@ -1,15 +1,17 @@
 @php
-	$restaurant_curr=collect($params)->where('post_type','restaurant')->last();
-	$restaurant_panel=Panel::get($restaurant_curr);
-	$openingHours=$restaurant_curr->openingHours;
-	
-	$opening_hour_edit_url=$restaurant_panel->relatedUrl(['related_name'=>'opening_hour','act'=>'index_edit']);
-	$opening_hour_create_url=$restaurant_panel->relatedUrl(['related_name'=>'opening_hour','act'=>'create']);
+$restaurant_curr = collect($params)
+    ->where('post_type', 'restaurant')
+    ->last();
+$restaurant_panel = Panel::get($restaurant_curr);
+$openingHours = $restaurant_curr->openingHours;
+
+$opening_hour_edit_url = $restaurant_panel->relatedUrl('opening_hour', 'index_edit');
+$opening_hour_create_url = $restaurant_panel->relatedUrl('opening_hour', 'create');
 @endphp
 @include($_layout->view_default.'.widgets.opening_hours',
-	[
-		'openingHours'=>$openingHours,
-		'edit_url'=>$opening_hour_edit_url,
-		'create_url'=>$opening_hour_create_url,
-	]
+[
+'openingHours'=>$openingHours,
+'edit_url'=>$opening_hour_edit_url,
+'create_url'=>$opening_hour_create_url,
+]
 )
